9da028da12126c1e328fe0e1c0f347740b0e1888
Zynh0722 <Zynh0722@gmail.com>
2022-12-31 05:33:14 -0800

    progress!

--- src/server/trpc/router/_app.ts
+++ src/server/trpc/router/_app.ts
@@ -1,10 +1,12 @@
 import { router } from "../trpc";
 import { authRouter } from "./auth";
 import { exampleRouter } from "./example";
+import { recipeRouter } from "./recipe";
 
 export const appRouter = router({
   example: exampleRouter,
   auth: authRouter,
+  recipe: recipeRouter,
 });
 
 // export type definition of API
