3f87021218e6e2f019b3900c6f760e018eedd319
Zynh0722 <Zynh0722@gmail.com>
2023-01-03 15:01:37 -0800

    housekeeping

--- src/components/RecipeMaker.tsx
+++ src/components/RecipeMaker.tsx
@@ -1,6 +1,5 @@
-import type { Recipe } from "@prisma/client";
 import { useState } from "react";
-import { trpc } from "../utils/trpc";
+// import { trpc } from "../utils/trpc";
 
 type RecipeWithUses = {
     id: string;
@@ -21,8 +20,8 @@ type Props = {
 
 export default function RecipeMaker({ makeRecipe }: Props) {
     const [input, setInput] = useState("");
-    const [editing, setEditing] = useState("");
-    const editMutation = trpc.recipe.updateRecipe.useMutation({});
+    // const [editing, setEditing] = useState("");
+    // const editMutation = trpc.recipe.updateRecipe.useMutation({});
 
     const submit = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
@@ -32,7 +31,13 @@ export default function RecipeMaker({ makeRecipe }: Props) {
     return <div className="">
         <form onSubmit={submit} className="flex flex-col justify-center">
             <input value={input} onChange={({ target: { value } }) => setInput(value)} className="bg-white/10 text-white py-2 px-6 rounded-full"></input>
-            <button type="submit" className="rounded-full m-2 bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20" onClick={() => { makeRecipe(input) }}>Make recipe!</button>
+            <button
+                type="submit"
+                className="rounded-full m-2 bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
+                onClick={() => { makeRecipe(input) }}
+            >
+                Make recipe!
+            </button>
         </form>
     </div>
 }
